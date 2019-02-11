a19a919ced561361c40897a7522bcd2227ee6ee2
Valentin <vad@surecave.com>
2019-02-10 19:25:41 -0500

    First models for roster

--- msf/roster/models.py
+++ msf/roster/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.db.models import CASCADE, OneToOneField, ForeignKey, ManyToManyField
+from django.forms import IntegerField
 
-# Create your models here.
+from msf_user.models import User
+from character.models import Character, Material
+
+
+class Roster(models.Model):
+    user = OneToOneField(User, on_delete=CASCADE)
+
+
+class CharacterInstance(models.Model):
+    roster = OneToOneField(Roster, on_delete=CASCADE)
+    character = ForeignKey(Character, on_delete=CASCADE)
+    level = IntegerField(
+        min_value=1,
+        max_value=70
+    )
+    stars = IntegerField(
+        min_value=1,
+        max_value=7
+    )
+    red_stars = IntegerField(
+        min_value=1,
+        max_value=7
+    )
+    gear_tier_level = IntegerField(
+        min_value=1,
+        max_value=13
+    )
+    current_gear_materials = ManyToManyField(
+        Material
+    )
+
