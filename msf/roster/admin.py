db06e0bd01bf606eff13c0803a63cf0ba553d6bc
Valentin <vad@surecave.com>
2019-02-16 15:51:58 -0500

    Update admin

--- msf/roster/admin.py
+++ msf/roster/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Roster, CharacterInstance
+
+admin.site.register(Roster)
+admin.site.register(CharacterInstance)
