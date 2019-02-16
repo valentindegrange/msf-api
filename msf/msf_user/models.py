7136cd3f5689f016701f8cbadd5bf0fae9ac6644
Valentin <vad@surecave.com>
2019-02-16 15:32:58 -0500

    Use AbstractUser as a base user instead of AbstractBaseUser

--- msf/msf_user/models.py
+++ msf/msf_user/models.py
@@ -1,11 +1,11 @@
-from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
 from django.db.models import CharField
 
 
-class User(AbstractBaseUser):
+class User(AbstractUser):
     username = CharField(unique=True, max_length=128)
 
     USERNAME_FIELD = 'username'
