37f9ab74e5b2bdfb3bcb2c291ea2460cc7f3ab3a
Valentin <vad@surecave.com>
2019-02-16 20:51:58 -0500

    Update RosterSerializer

--- msf/api/serializers/rosters.py
+++ msf/api/serializers/rosters.py
@@ -6,8 +6,9 @@ from roster.models import Roster
 
 
 class RosterSerializer(ModelSerializer):
-
+    user_id = serializers.CharField(source='user.id', read_only=True)
+    
     class Meta:
         model = Roster
-        fields = ('shareable', 'id')
-        read_only_fields = ('id',)
+        fields = ('shareable', 'id', 'user_id')
+        read_only_fields = ('id', 'user_id')
